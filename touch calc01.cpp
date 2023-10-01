7e23370eb14349d02194918a2eb51b9067ff2ba0
victooiya <145699319+victooiya@users.noreply.github.com>
2023-10-01 16:13:59 +0300

    Add files via upload

---
+++ touch calc01.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+int a, b, op;   double result;
+cout << "Введіть перше число\n";   cin >> a;
+cout << "Введіть оператор\n1\t+\n2\t-\n3\t*\n4\t/\n\n";  cin >> op;
+cout << "Введіть друге число\n";     cin >> b;
+switch(op) {
+  case 1:    result = a + b;  break;
+  case 2:    result = a - b;  break;
+  case 3:    result = a * b;  break;
+  case 4:    result = a / b;  break;
+  case 5:    result = pow(a,b); break;
+}
+cout << "Результат = " << result;
+}
